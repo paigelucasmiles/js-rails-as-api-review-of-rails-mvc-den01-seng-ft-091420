fd1f21e3b37dd35e5027477153f0cdce9d961bbe
Paige Miles <paige@lucas-miles.com>
2020-10-13 01:14:59 +0000

    Automatically backed up by Learn

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,4 +1,7 @@
 # Add code from Readme
 class Bird < ApplicationRecord
+<<<<<<< HEAD
     
+=======
+>>>>>>> b721a679aa1e8f4145a5d3b98bbe1d9234ad8a64
 end
--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class CreateBirds < ActiveRecord::Migration[5.2]
     def change
       create_table :birds do |t|
@@ -8,3 +9,16 @@ class CreateBirds < ActiveRecord::Migration[5.2]
       end
     end
   end
+=======
+# Add code from Readme
+class CreateBirds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :birds do |t|
+      t.string :name
+      t.string :species
+ 
+      t.timestamps
+    end
+  end
+end
+>>>>>>> b721a679aa1e8f4145a5d3b98bbe1d9234ad8a64
